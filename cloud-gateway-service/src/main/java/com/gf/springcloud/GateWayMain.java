00daabf3380a1f9b88a02896685c3859eeb47f22
guyt <tao.gu@jobzt.cn>
2022-04-10 22:35:58 +0800

    网关配置

---
+++ cloud-gateway-service/src/main/java/com/gf/springcloud/GateWayMain.java
@@ -0,0 +1,15 @@
+package com.gf.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@EnableCircuitBreaker
+@SpringBootApplication
+public class GateWayMain
+{
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain.class, args);
+    }
+}
